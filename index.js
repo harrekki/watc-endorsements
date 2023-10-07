8e13dafa9a3bac73324fda69201dfacd30288b20
David LaRocco <dlarocco77@gmail.com>
2023-10-07 11:04:18 -0600

    add realtime endorsements to UI

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { initializeApp } 
     from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref } 
+import { 
+    getDatabase, 
+    ref,
+    push,
+    onValue     
+} 
     from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"; 
 
 const appSettings = {
@@ -12,12 +17,50 @@ const endorsementsDB = ref(database, "endorsements");
 
 const inputField = document.getElementById("endorse-field");
 const publishBtn = document.getElementById("publish-btn");
+const endorsementsList = document.getElementById("endorsements-wrapper");
 
 publishBtn.addEventListener("click", addToDatabase);
 
 function addToDatabase() {
     let inputValue = inputField.value;
 
-    console.log(`${inputValue} added to database`);
+    if(inputValue) {
+        push(endorsementsDB, inputValue);
+    }
+
+    clearInputField();
+}
+
+onValue(endorsementsDB, function(snapshot) {
+    if(snapshot.exists()) {
+        let endorsementsArray = Object.entries(snapshot.val());
+        
+        clearEndorsementsList();
+
+        for(let endorsement of endorsementsArray) {
+            appendToEndorsementsList(endorsement);
+        }
+    }
+})
+
+function clearInputField() {
+    inputField.value = "";
+}
+
+function clearEndorsementsList() {
+    endorsementsList.innerHTML = "";
+}
+
+function appendToEndorsementsList(item) {
+    let itemID = item[0];
+    let itemValue = item[1];
+
+    let newElem = `
+        <article>
+            ${itemValue}
+        </article>
+    `;
+
+    endorsementsList.innerHTML += newElem;
 }
 
